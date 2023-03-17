fefa4c19f5e76d254b6ee805afadcfd9abcd7073
Mirian Santana <miriansantana93@gmail.com>
2023-03-17 12:30:48 -0300

    Added class ScreenValueViewController

--- ProjectRequestPizzaThree/ProjectRequestPizzaThree/ScreenRatingViewController.swift
+++ ProjectRequestPizzaThree/ProjectRequestPizzaThree/ScreenRatingViewController.swift
@@ -17,12 +17,21 @@ class ScreenRatingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        lblNamePizza.text = "De acordo com nossos clientes, a quantidade de estrelas que a pizza de \(rating?.name ?? "") possui é de: "
+        lblRating.text = "\(rating?.rating ?? 0)"
     }
     
     @IBAction func btValues(_ sender: Any) {
+        if let screen = self.storyboard?.instantiateViewController(withIdentifier: "value") as? ScreenValueViewController {
+            
+            screen.value = self.rating
+            
+            self.navigationController?.pushViewController(screen, animated: true)
+        }
     }
     
     @IBAction func btBack(_ sender: Any) {
+        
+        self.navigationController?.popViewController(animated: true)
     }
 }
---
+++ ProjectRequestPizzaThree/ProjectRequestPizzaThree/ScreenValueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScreenValueViewController.swift
+//  ProjectRequestPizzaThree
+//
+//  Created by Mirian Santana on 17/03/23.
+//
+
+import UIKit
+
+class ScreenValueViewController: UIViewController {
+    
+    @IBOutlet weak var lblPizzaP: UILabel!
+    @IBOutlet weak var lblPizzaM: UILabel!
+    @IBOutlet weak var lblPizzaG: UILabel!
+    
+    var value: PizzaElement?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        lblPizzaP.text = "PEQUENA - R$\(value?.priceP ?? 0.0)"
+        lblPizzaM.text = "MÉDIA - R$\(value?.priceM ?? 0.0)"
+        lblPizzaG.text = "GRANDE - R$\(value?.priceG ?? 0.0)"
+    }
+ 
+    @IBAction func btBack(_ sender: Any) {
+        
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+}
